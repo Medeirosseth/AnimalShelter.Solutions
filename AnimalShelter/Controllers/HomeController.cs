1fa0b18997f93ca0fc9cf02acc247bcc0c18fb50
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-18 09:32:50 -0700

    add initial boiler plate
    Co-Authored-by: Min Chang <minchangmhc@gmail.com>

---
+++ AnimalShelter/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+
+namespace PierresBakeryMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+    }
+}
